fb7ec8ac33b316afd7e63c8cfbb4cba782283c15
punitcodes <punit.makwana25@gmail.com>
2020-11-22 17:02:50 +0530

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    description:
+      "Frontend Developer specializing in React and its framework (Nextjs & Gatsby).",
+    locale: "en",
+    title: "Punit Makwana - ReactJS Developer",
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        showThemeLogo: false,
+        theme: "classic",
+      },
+    },
+  ],
+};
